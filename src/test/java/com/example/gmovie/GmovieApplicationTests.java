d554a05f2c87f736ab3d1301713dd172b62a816a
574670 <makaeron20@gmail.com>
2021-03-20 11:07:14 -0400

    intial commit

---
+++ src/test/java/com/example/gmovie/GmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
